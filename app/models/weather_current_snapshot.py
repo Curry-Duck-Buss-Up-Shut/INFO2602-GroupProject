b6559cc8de1123ea2e2e5c677c00e7c2e1906625
areanna-ali <areannaali76@gmail.com>
2026-04-14 16:57:45 -0400

    THANK GOD!

--- app/models/weather_current_snapshot.py
+++ app/models/weather_current_snapshot.py
@@ -1,8 +1,6 @@
 from datetime import datetime, timezone
 from typing import Optional
-
 from sqlmodel import Field, SQLModel
-
 
 class WeatherCurrentSnapshot(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
