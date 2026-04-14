fdab015e0b225a4a2f860ef2441553bfba9be57d
SachinUWIRepo <sachin.lee15@gmail.com>
2026-04-14 00:26:47 -0400

    final final commit

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,5 @@
-from .user import User
-from .saved_location import SavedLocation
 from .disaster_event import DisasterEvent
+from .saved_location import SavedLocation
+from .user import User
 
 __all__ = ["User", "SavedLocation", "DisasterEvent"]
