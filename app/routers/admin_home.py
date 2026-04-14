39e495daae60ee0724e5e4f9611ccfa40a1032e0
SachinUWIRepo <sachin.lee15@gmail.com>
2026-04-14 00:07:55 -0400

    final fixes commit - Sachin

--- app/routers/admin_home.py
+++ app/routers/admin_home.py
@@ -1,8 +1,7 @@
-from fastapi import APIRouter, HTTPException, Depends, Request
-from fastapi.responses import HTMLResponse, RedirectResponse
-from fastapi import status
-from app.dependencies.session import SessionDep
-from app.dependencies.auth import AdminDep, IsUserLoggedIn, get_current_user, is_admin
+from fastapi import Request
+from fastapi.responses import HTMLResponse
+
+from app.dependencies.auth import AdminDep
 from . import router, templates
 
 
@@ -10,7 +9,6 @@ from . import router, templates
 async def admin_home_view(
     request: Request,
     user: AdminDep,
-    db:SessionDep
 ):
     return templates.TemplateResponse(
         request=request, 
